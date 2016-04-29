060ebc76695e7864f308b16ca7d438bfd8954384
snreis <sandranogueirareis@gmail.com>
2016-04-29 11:28:56 +0100

    updated version of cut_stamps_astlib
    The program was for one galaxy. Now it is possible to run it automatically for as many galaxies as I want

--- cut_stamps_astlib.py
+++ cut_stamps_astlib.py
@@ -1,7 +1,7 @@
 ########################################################
 # Cut a galaxy from a FITS file with an updated header #
 # Author: S. N. dos Reis                               #
-# Date: April 2016									                   #
+# Date: April 2016									                #
 ########################################################
 
 import pyfits
@@ -12,11 +12,11 @@ from astLib import astWCS
 
 
 #constants
-my_galaxy = '1'
+lim = 99
 
 #Read the output table from SExtractor
 tbl   = "gs.cat"
-table = np.loadtxt(tbl)
+gal,xx,yy,mag_aper,flux_aper,fluxerr_aper,flux_auto,fluxerr_auto,ra,dec,fwhm,sm_sex,xmin,xmax,ymin,ymax = np.loadtxt(tbl, unpack = True)
 
 #Open and read
 hdulist = pyfits.open("gs_all_candels_ers_udf_f160w_060mas_v0.5_drz.fits")
@@ -26,19 +26,18 @@ img = hdulist[0].data
 #Create wcs object
 wcs = astWCS.WCS(hdr, mode = "pyfits")
 
-#Read the position of the galaxy
-xx = table[0,1]
-yy = table[0,2]
+for ii in range(len(gal)):
+    if ii <= lim:
+      #Transform from pixels to decimal
+      coords = astWCS.WCS.pix2wcs(wcs,xx[ii],yy[ii])
+      ra     = coords[0]
+      dec    = coords[1]
 
-#Transform from pixels to decimal
-coords = astWCS.WCS.pix2wcs(wcs,xx,yy)
-ra     = coords[0]
-dec    = coords[1]
+      #cut the stamp
+      my_new_image = astImages.clipImageSectionWCS(img, wcs, ra, dec, 0.001667, returnWCS=True)
 
-#cut the stamp
-my_new_image = astImages.clipImageSectionWCS(img, wcs, ra, dec, 0.001667, returnWCS=True)
-
-#write FITS file
-data     = my_new_image['data']
-imagewcs = my_new_image['wcs']
-astImages.saveFITS(my_galaxy+".fits", data, imagewcs)
+      #write FITS file
+      data     = my_new_image['data']
+      imagewcs = my_new_image['wcs']
+      iii = ii + 1
+      astImages.saveFITS('%s'%iii+".fits", data, imagewcs)
